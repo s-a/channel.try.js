8362de0bb0cffeff3c6c6a08da1fc5785b470311
s-a <stephan.ahlf@googlemail.com>
2015-02-05 09:27:06 +0100

    added jshint validation

--- test.js
+++ test.js
@@ -1,18 +1,32 @@
  var
+	fs = require("fs"),
 	assert = require("assert"),
-	should = require("should");
+	should = require("should"),
+	JSHINT = require("jshint").JSHINT,
+	source = fs.readFileSync("./channel.json").toString(),
+	options = JSON.parse(fs.readFileSync("./.jshintrc", "utf8"));
 
 
-describe('test1', function(){
-	it('test2', function(done){
-		var res = 1
-		res.should.equal(1);
+
+
+describe('channel.json should be valid against .jshintrc', function(){
+	
+	it('should have no errors', function(done){
+
+		JSHINT(source, options);
+		var report = JSHINT.data();
+
+		should.not.exist(report.errors);
+		if (report.errors){
+			//report.errors.length.should.equal(0);
+			//for (var i = 0; i < report.errors.length; i++) {
+			//	var err = report.errors[i];
+			  	//err.reason.should.equal("");
+			//}
+		}
+
+
 		done();
 	});
 
-	it('test3', function(done){
-		var res = 1
-		res.should.equal(1);
-		done();
-	});
 });
